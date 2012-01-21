5ef48991227a2a99cf69e4c8183be79cdd10a625
James Kirkwood <kirkwojl@hotmail.com>
2012-01-21 13:43:31 -0500

    implemented tests and bug fixes for 'at' command

--- test/remindme/test/core.clj
+++ test/remindme/test/core.clj
@@ -14,7 +14,15 @@
     (is (= (.getHourOfDay target) 14))
     (is (= (.getMinuteOfHour target) 25))))
 
-(defn- joda-to-millis [jt] (.getTime (.toDate jt)))
+(defn- now-plus
+  "Returns joda LocalDateTime for the specified number of minutes added to the current time"
+  [mins]
+  (.plusMinutes (LocalDateTime.) mins))
+
+(defn- time-spec
+  "Returns the current joda time as 'hh:mi:aa' format used in at commands"
+  [ldt]
+  (.print (DateTimeFormat/forPattern "hh:mmaa") ldt))
 
 (deftest test-at-already-executed
   (binding [*last-execution* (today-at "5:00am")]
@@ -22,7 +30,21 @@
   (binding [*last-execution* (today-at "4:00am")]
     (is (at "4:55am"))))
 
-;(deftest test-at-not-yet
-;(deftest test-at-ready
-;(deftest test-at-ready-with-additional
+(deftest test-at-not-yet
+  (binding [*last-execution* (today-at "12:01am")]
+    (let [ten-min-ahead-string (time-spec (now-plus 10))]
+    ; need to get a few minutes back so that the at time is
+      (is (not (at ten-min-ahead-string))))))
+
+(deftest test-at-ready
+  (binding [*last-execution* (today-at "12:01am")]
+    (let [ten-min-back-string (time-spec (now-plus -10))]
+      (is (at ten-min-back-string)))))
+
+(deftest test-at-ready-with-additional
+  (binding [*last-execution* (today-at "12:01am")]
+    (let [ten-min-back-string (time-spec (now-plus -10))]
+      (is (not (at ten-min-back-string false)))
+      (is (not (at ten-min-back-string (< 10 5))))
+      (is (at ten-min-back-string (< 5 10))))))
 
